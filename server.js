43fca7bf2314c7e30f43459f8be2beb7bf6b64ad
Israel Rabassa <idrabassa@gmail.com>
2021-04-04 12:51:46 -0400

    update express  elements

--- server.js
+++ server.js
@@ -1,11 +1,18 @@
 const express = require("express")
 const dotenv = require("dotenv")
+const logger=require('./middleware/logger')
+// Routes files
+const bootcamps = require('./routes/bootcamps')
 
 //Load env vars
-
 dotenv.config({path:'./config/config.env'})
 
 const app =express()
+
+app.use(logger )
+// Mount routers
+app.use('/api/v1/bootcamps',bootcamps);
+
 const PORT =process.env.PORT || 5000
 
 app.listen(PORT, () => {

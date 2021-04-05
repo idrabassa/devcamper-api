adc157e000c5fc1d02b20e6e8f665df7bd76c465
Israel Rabassa <idrabassa@gmail.com>
2021-04-05 15:23:46 -0400

    update express  elements

--- controllers/bootcamps.js
+++ controllers/bootcamps.js
@@ -15,8 +15,16 @@ exports.getBootcamps = async (req,res,next)=>{
 //@desc    Get single bootcamp
 //@route   Get  /api/v1/bootcamps/:id
 //@access  Public
-exports.getBootcamp = (req,res,next)=>{
-    res.status(200).json({success:true,msg:`Get the bootcamp ${req.params.id} `}) 
+exports.getBootcamp = async (req,res,next)=>{
+    try {
+        const bootcamp= await Bootcamp.findById(req.params.id)
+        if (!bootcamp) {
+            res.status(400).json({success:false})
+        }
+        res.status(200).json({success:true,data:bootcamp}) 
+    } catch (error) {
+        res.status(400).json({success:false})
+    }
 }
 
 //@desc    Create new bootcamp

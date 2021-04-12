e5e846bcd0203b326358a2ea96710f3841152ce6
Israel Rabassa <idrabassa@gmail.com>
2021-04-12 16:48:11 -0400

    update express  elements

--- seeder.js
+++ seeder.js
@@ -9,6 +9,7 @@ dotenv.config({path:'./config/config.env'})
 //Load models
 
 const Bootcamp =require('./modules/Bootcamp')
+const Course =require('./modules/Course')
 
 //connect to db
 
@@ -21,12 +22,14 @@ const conn =mongoose.connect(process.env.MONGO_URI,{
 
 //read the JSON files
 const bootcamps =JSON.parse(fs.readFileSync(`${__dirname}/_data/bootcamps.json`,'utf-8'))
+const courses =JSON.parse(fs.readFileSync(`${__dirname}/_data/courses.json`,'utf-8'))
 
 //import into DB
 
 const importData = async ()=>{
     try {
         await Bootcamp.create(bootcamps)
+        await Course.create(courses)
         console.log('Data imported ...'.green.inverse);
         process.exit()
     } catch (error) {
@@ -37,6 +40,7 @@ const importData = async ()=>{
 const deleteData = async ()=>{
     try {
         await Bootcamp.deleteMany()
+        await Course.deleteMany()
         console.log('Data destroyed ...'.red.inverse);
         process.exit()
     } catch (error) {

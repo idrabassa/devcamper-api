647fe5bc119b99259ddf5b1bb159567c3ebae5c0
Israel Rabassa <idrabassa@gmail.com>
2021-04-13 17:23:41 -0400

    update express  elements

--- routes/courses.js
+++ routes/courses.js
@@ -1,7 +1,7 @@
 const express = require('express')
-const {getCourses,getCourse} = require('../controllers/courses')
+const {getCourses,getCourse,addCourse,updateCourse} = require('../controllers/courses')
 const router = express.Router({mergeParams:true})
-router.route('/').get(getCourses)
-router.route('/:id').get(getCourse)
+router.route('/').get(getCourses).post(addCourse)
+router.route('/:id').get(getCourse).put(updateCourse)
 // router.route('/:id').get(getBootcamp).put(updateBootcamp).delete(deleteBootcamp)
 module.exports = router;

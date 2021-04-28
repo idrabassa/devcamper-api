ec1fe7ddb3ac5309b38d48beb9ea0fa1deb27523
Israel Rabassa <idrabassa@gmail.com>
2021-04-28 18:09:36 -0400

    update express  elements

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,5 +1,6 @@
 const User = require("../modules/User");
 const asyncHandler=require('../middleware/async')
+const sendEmail = require("../utils/sendEmail")
 //const errorResponse=require('../utils/errorResponse');
 const ErrorResponse = require("../utils/errorResponse");
 
@@ -53,31 +54,6 @@ exports.login = asyncHandler(async (req, res, next) => {
     sendTokenResponse(user, 200, res);
   });
 
-//Get token from the model, create cookie and send response
-const sendTokenResponse = (user, statusCode, res) => {
-    // Create token
-    const token = user.getSignedJwtToken();
-  
-    const options = {
-      expires: new Date(
-        Date.now() + process.env.JWT_COOKIE_EXPIRE * 24 * 60 * 60 * 1000
-      ),
-      httpOnly: true
-    };
-  
-    if (process.env.NODE_ENV === 'production') {
-      options.secure = true;
-    }
-  
-    res
-      .status(statusCode)
-      .cookie('token', token, options)
-      .json({
-        success: true,
-        token
-      });
-  };
-
 // @desc      Get current logged in user
 // @route     POST /api/v1/auth/me
 // @access    Private
@@ -90,3 +66,67 @@ exports.getMe = asyncHandler(async (req, res, next) => {
     });
   });
 
+// @desc      Forgot password
+// @route     POST /api/v1/auth/forgotpassword
+// @access    Public
+exports.forgotPassword = asyncHandler(async (req, res, next) => {
+  const user = await User.findOne({email: req.body.email});
+  
+  if(!user){
+    next( new ErrorResponse('There is no user with that email'),404)
+  }
+ //get reset token
+ const resetToken = user.getResetPasswordToken()
+ await user.save({validateBeforeSave:false})
+
+ //create reset url
+ const resetUrl=`${req.protocol}://${req.get('host')}/api/v1/resetpassword/${resetToken}` 
+
+ const message =`You are receiving this email because you (or somone else) has requested the reset of the password. Please make sure a PUT request to: \n\n ${resetUrl}`
+ try {
+  await sendEmail({
+    email: user.email,
+    subject: 'Password reset token',
+    message
+  });
+
+  res.status(200).json({ success: true, data: 'Email sent' });
+} catch (err) {
+  console.log(err);
+  user.resetPasswordToken = undefined;
+  user.resetPasswordExpire = undefined;
+
+  await user.save({ validateBeforeSave: false });
+
+  return next(new ErrorResponse('Email could not be sent', 500));
+}
+  res.status(200).json({
+    success: true,
+    data: user
+  });
+});
+
+//Get token from the model, create cookie and send response
+const sendTokenResponse = (user, statusCode, res) => {
+  // Create token
+  const token = user.getSignedJwtToken();
+
+  const options = {
+    expires: new Date(
+      Date.now() + process.env.JWT_COOKIE_EXPIRE * 24 * 60 * 60 * 1000
+    ),
+    httpOnly: true
+  };
+
+  if (process.env.NODE_ENV === 'production') {
+    options.secure = true;
+  }
+
+  res
+    .status(statusCode)
+    .cookie('token', token, options)
+    .json({
+      success: true,
+      token
+    });
+};
